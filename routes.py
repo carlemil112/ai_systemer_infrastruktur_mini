b4011a2aa460d0a336e0a405628b1b45a9766bef
carlemil112 <carl.emil.hejslet@gmail.com>
2025-11-26 12:46:35 +0100

    sync

--- routes.py
+++ routes.py
@@ -1,35 +1,41 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from database import SessionLocal
-from database import RequestLog
-from security import get_current_api_key  # er igang med at lave
+
 from models import ImageRequest, ReviewRequest, ClassificationResult
-from semantic import SemanticReviewClassifier # skal tilpasses vores modeller
+from semantic import SemanticReviewClassifier
 from imageclassifier import ImageClassifier
 
+#database + api nøgler
+from database import get_db, RequestLog
+from security import get_current_api_key
 
 router = APIRouter(prefix="/v1")
 
 review_classifier = SemanticReviewClassifier()
 image_classifier = ImageClassifier()
-# Navne afhængigt af hvordan vores modeller ser ud. Mangler error handling.
+
+
 @router.post("/classify-image", response_model=ClassificationResult)
-def classify(req: ImageRequest):
+def classify_image(
+    req: ImageRequest,
+    db: Session = Depends(get_db),
+    api_key: str = Depends(get_current_api_key),
+):
+
     label, conf = image_classifier.classify_image(req.image_base64)
-    return ClassificationResult(label=label, confidence=conf)
-#Afhængigt af hvilke modeller vi bruger. 
-@router.post("/classify-review", response_model=ClassificationResult)
-def classify_review(req: ReviewRequest):
-    label, conf = review_classifier.classify_review(req.text)
-    return ClassificationResult(label=label, confidence=conf)
 
+    # log i databasen (gemmer ikke hele billedet, kun output)
+    log = RequestLog(
+        api_key=api_key,
+        endpoint="classify-image",
+        input_text=None,
+        predicted_label=label,
+        confidence=conf,
+    )
+    db.add(log)
+    db.commit()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    return ClassificationResult(label=label, confidence=conf)
 
 
 @router.post("/classify-review", response_model=ClassificationResult)
@@ -40,6 +46,7 @@ def classify_review(
 ):
     label, conf = review_classifier.classify_review(req.text)
 
+    # log request+output
     log = RequestLog(
         api_key=api_key,
         endpoint="classify-review",
