977cb1dcdf1a387fa30f159615cb34babe4182bd
Mathilde Vibæk <mathildevibaek@hotmail.com>
2025-12-01 14:51:17 +0100

    Update BASE_URL to new server address

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ from PIL import Image
 import base64
 
 #Hvor serveren kører (localthost)
-BASE_URL = "http://172.20.10.2:8000"
+BASE_URL = "http://10.27.12.29:8000"
 #Hvordan den logger ind på serveren (her med en API-nøgle)
 API_KEY = "secret123" 
 #Hvad den sender med hver forespørgsel til serveren
